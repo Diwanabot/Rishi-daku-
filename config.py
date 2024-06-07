cc13f19e940fd2f7d95fc79de35e480d83533b39
Diwanabot <156938668+Diwanabot@users.noreply.github.com>
2024-06-07 10:19:36 +0530

    Update config.py
    Signed-off-by: Diwanabot <156938668+Diwanabot@users.noreply.github.com>

--- config.py
+++ config.py
@@ -39,8 +39,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/theyourAnnu881")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/trsbotupdatess")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/damanoffcialpredictionn")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/damanoffcialpredictionn")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", "5400"))
@@ -78,13 +78,13 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://graph.org/file/c65fa9ccf4d292ea599e3.mp4"
+    "START_IMG_URL", "https://telegra.ph/file/832b361cacec6702eb99d.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL" , "https://graph.org/file/c65fa9ccf4d292ea599e3.mp4"
+    "PING_IMG_URL" , "https://telegra.ph/file/832b361cacec6702eb99d.jpg"
 )
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
-STATS_IMG_URL = "https://graph.org/file/c65fa9ccf4d292ea599e3.mp4"
+STATS_IMG_URL = "https://telegra.ph/file/832b361cacec6702eb99d.jpg"
 TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
 TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
 STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
